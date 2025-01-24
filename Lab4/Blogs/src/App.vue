020fd97bea1c8cf939f08d1ac81f3702fe431dd7
fazlibeqir <fazlibeqir@gmail.com>
2025-01-24 21:55:43 +0100

    Lab4

---
+++ Lab4/Blogs/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+//imports
+import BlowView from './components/BlogView.vue';
+</script>
+
+<template>
+<!-- Views -->
+ <BlowView/>
+</template>
+
+<style>
+#app{
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+input{
+  padding: 10px;
+  margin: 10px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+}
+</style>
